5d93e0221b3131e485ec4936010c46e3a3093ca9
Sivasish Praharaj <98220976+Sivasish48@users.noreply.github.com>
2023-11-07 23:42:54 +0530

    Update arrow05.js

--- Basics03/arrow05.js
+++ Basics03/arrow05.js
@@ -5,7 +5,7 @@
 
 // This keyword refers to different objects depending on how it is used.
 
-// let us see a example of this and how it refers to the object.
+// let us see an example of this and how it refers to the object.
 
 const user = {
     userName : "Suvam" ,
@@ -18,18 +18,18 @@ const user = {
 console.log(user); // { userName: 'Suvam', price: 399, message: [Function: message] 
 console.log(user.message()); // Suvam , welcome.
 
-// lets change the value of the property
+//let's change the value of the property
 
 user.userName = "Peps"
-console.log(user.message()); // Peps , welcome ( here the current context of the keyword userName is"Peps")
+console.log(user.message()); // Peps, welcome ( here the current context of the keyword userName is"Peps")
 
-// now we got to know that "this" keyword refers to current context of the scope.
+// now we know that the "this" keyword refers to the current context of the scope.
 
-//  so we can say is "In an object method, this refers to the object."
+//  so we can say "In an object method, this refers to the object."
 
 // In a function, this refers to the global object.
 
-// here we have the user object where we have a property called as message and it stores a function and in the function this keyword is used so this keyword will refer to its global object that is user object.
+//Here we have the user object where we have a property called message and it stores a function in the function, this keyword is used so this keyword will refer to its global object that is user object.
 
 // example
  
@@ -46,9 +46,9 @@ console.log(userN.msg()); // { name: 'Sivasish', msg: [Function: msg] }
 
 // So here the output is the current context we we print the"this" keyword.
 
-// let us see if we console log the this keyword in the global scope or globally.
+//Let us see if we console log this keyword in the global scope or globally.
 
-console.log(this); // {} (it shows empty. because we are in the node js environment where there no object means empty object)
+console.log(this); // {} (it shows empty. because we are in the node js environment where there is no object means empty object)
 
 
 // BUT >>IMPORTANT<<
@@ -58,7 +58,65 @@ console.log(this); // {} (it shows empty. because we are in the node js environm
 
 // So we got to know that 
 // Alone, this refers to the global object.
-// here in node environment there no object or empty object is present.
+//Here in the node environment no object or empty object is present.
 
-// but in the browser there is window object which is a global object in the browser.
+// but in the browser there is a window object which is a global object in the browser.
 
+// Let us see if we console log "this" in a function
+
+function ai(){
+    console.log(this);
+}
+ai()
+/*
+<ref *1> Object [global] {
+  global: [Circular *1],
+  queueMicrotask: [Function: queueMicrotask],
+  clearImmediate: [Function: clearImmediate],
+  setImmediate: [Function: setImmediate] {
+    [Symbol(nodejs.util.promisify.custom)]: [Getter]
+  },
+  structuredClone: [Function: structuredClone],
+  clearInterval: [Function: clearInterval],
+  clearTimeout: [Function: clearTimeout],
+  setInterval: [Function: setInterval],
+  setTimeout: [Function: setTimeout] {
+    [Symbol(nodejs.util.promisify.custom)]: [Getter]
+  },
+  atob: [Function: atob],
+  btoa: [Function: btoa],
+  performance: Performance {
+    nodeTiming: PerformanceNodeTiming {
+      name: 'node',
+      entryType: 'node',
+      startTime: 0,
+      duration: 87.89910000562668,
+      nodeStart: 2.723200000822544,
+      v8Start: 6.244800001382828,
+      bootstrapComplete: 43.40700000524521,
+      environment: 17.5912000015378,
+      loopStart: -1,
+      loopExit: -1,
+      idleTime: 0
+    },
+    timeOrigin: 1699330047657.564
+  },
+  fetch: [AsyncFunction: fetch]
+}
+*/
+
+//These are the value of this keyword as a global object.
+
+// The values of the global object in Node.js are functions and variables that are available to all modules in your Node.js program. This means that you can access them anywhere in your program without having to import them.
+
+//let's check if the "this" keyword also refers to the current context while in the function or not.
+
+function hi(){
+    let user = "suvam"
+    console.log(this.user);
+
+}
+
+hi() // undefined
+
+//Now we got to know that reference for the current context using this doesn't work in the function.
