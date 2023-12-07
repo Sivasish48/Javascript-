36d8aa3ddf7166cb7a311769a0be93ac665dcdbb
Sivasish Praharaj <98220976+Sivasish48@users.noreply.github.com>
2023-12-07 10:59:25 +0530

    forOfLoops.js

---
+++ Iterations05/forOfLoops.js
@@ -0,0 +1,46 @@
+// for of 
+
+// These are array specific loops
+
+// The JavaScript for of statement loops through the values of an iterable object.
+
+// It lets you loop over iterable data structures such as Arrays, Strings, Maps, NodeLists, and more
+
+const arr = [98,78,657,980,23]
+
+for (const i of arr) {
+    console.log(`numbers  are ${i}`);
+    
+}
+
+/*
+
+numbers  are 98
+numbers  are 78
+numbers  are 657
+numbers  are 980
+numbers  are 23
+*/
+
+
+// WE can also use in string
+
+const msg ='Namaste'
+
+for (const i of msg) {
+    console.log(`messages are ${i} `);
+    
+}
+
+/*
+
+messages are N
+messages are a
+messages are m
+messages are a
+messages are s
+messages are t
+messages are e
+
+*/
+
